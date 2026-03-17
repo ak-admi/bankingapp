4b56991f26bedcd02c2538d277aebf40d6d7ab07
ak-admi <hangarialok@gmail.com>
2026-03-17 22:10:52 +0530

    started modifying UserRequest Dto to handle request validation

--- src/main/java/com/alok/bankingapp/dto/UserRequest.java
+++ src/main/java/com/alok/bankingapp/dto/UserRequest.java
@@ -1,12 +1,38 @@
 package com.alok.bankingapp.dto;
 
+import jakarta.validation.constraints.*;
+
 public class UserRequest {
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
     private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
 
-    public String getName(){return name;}
+    public UserRequest() {
+    }
+
+    public UserRequest(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public String getEmail() {
         return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
